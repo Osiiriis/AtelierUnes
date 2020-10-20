b9ffc3dfefd4b6d6cebba97651f4201bba73d2f8
DeLagardePro <prodelagarde@gmail.com>
2020-10-20 10:44:48 +0200

    build

---
+++ build/precache-manifest.63cb33111a0fd1c4cebb90ac3111f4d2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "720742f0f874afc48c880bf974159edc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22eaf5bc3551e3e0c6a5",
+    "url": "/static/css/main.f8908548.chunk.css"
+  },
+  {
+    "revision": "dfd3e7373e663c2222f3",
+    "url": "/static/js/2.64328cf2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.64328cf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22eaf5bc3551e3e0c6a5",
+    "url": "/static/js/main.f76d9bba.chunk.js"
+  },
+  {
+    "revision": "e5adbc4f6484e7046984",
+    "url": "/static/js/runtime-main.75ae284a.js"
+  },
+  {
+    "revision": "e614772868ce9746774194a4ec2a999d",
+    "url": "/static/media/accordeonThree.e6147728.jpg"
+  },
+  {
+    "revision": "a5f06e2a97572b24b0b6e48fca7cb83f",
+    "url": "/static/media/accordeonTwo.a5f06e2a.jpg"
+  },
+  {
+    "revision": "f450209d7307a890ef453f06d512f43a",
+    "url": "/static/media/color.f450209d.png"
+  },
+  {
+    "revision": "8c394e932b550c391f06ae2fa0f09541",
+    "url": "/static/media/femme.8c394e93.jpg"
+  },
+  {
+    "revision": "b735ac028789b0bd217f47a2edbfdb7c",
+    "url": "/static/media/ocre.b735ac02.jpg"
+  }
+]);
